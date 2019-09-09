4c7a38bfd099fc4dd420875eabeb7642af02fff3
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-09 19:57:42 +0100

    Services and injectables

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -33,7 +33,7 @@ import { Component, Input } from "@angular/core";
 export class EventThumbnailComponent {
     @Input() event: any
 
-    getStartTimeStyle(): Object {
+    getStartTimeStyle(): any {
         if (this.event && this.event.time === '8:00 am') {
             return { color: "#003300", 'font-weight': "bold" };
         }
