72f9a9aa816ff3974ecd00f47fd158eed9bd0d98
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-13 00:23:25 +0100

    Reactive forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+    em {color: red; float: right}
+    `]
 })
 
 export class LoginComponent {
     password: any
     userName: any
+    mouseOverLogin: boolean
+
+    constructor(private authService: AuthService,
+        private router: Router) {
+
+    }
+
     login(formValues) {
-        console.log(formValues)
+        this.authService.loginUser(formValues.userName,
+            formValues.password)
+        this.router.navigate(['events'])
+    }
+    cancel() {
+        this.router.navigate(['events'])
     }
 }
